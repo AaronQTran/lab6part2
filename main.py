4610d811f5ec547c81a1411c28dcbc6cf7890ae4
PhilStef <philstefanowicz@aol.com>
2023-10-24 19:16:29 -0400

    push from phillip

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 def main():
     boo = True
-
-    print('Menu')
-    print('-------------')
-    print('1. Encode')
-    print('2. Decode')
-    print('3. Quit')
-    user_input = int(input('Please enter an option: '))
+    pass_word = ""
 
 
     while boo:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit\n')
+        user_input = int(input('Please enter an option: '))
         if user_input == 1:
             password = input('Please enter your password: ')
-            print('Your password has been encoded and stored!')
+            print('Your password has been encoded and stored!\n')
             pass_word = encode(password)
-        elif user_input == 2:
-            print('The encoded password is', pass_word, 'and the original password is', password)
+        if user_input == 2:
+            print('The encoded password is', pass_word, 'and the original password is', decode(pass_word), ".\n")
+        if user_input == 3:
+            break
 
-        
-        
 
 def encode(password):
     temp_string = ''        
@@ -27,3 +27,16 @@ def encode(password):
         i += 3
         temp_string += str(i)
     return temp_string
+
+
+def decode(password):
+    temp_string = ''
+    for i in password:
+        i = int(i)
+        i -= 3
+        temp_string += str(i)
+    return temp_string
+
+
+if __name__ == "__main__":
+    main()
