8fb42d1efbd76ac5a7cd59534e09dc2edbbcc776
Aaron Tran <dahpaladin123@gmail.com>
2023-10-24 18:38:29 -0400

    added main menu and encode

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-print('hi')
-print('yooooo')
+def main():
+    boo = True
+
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    user_input = int(input('Please enter an option: '))
+
+
+    while boo:
+        if user_input == 1:
+            password = input('Please enter your password: ')
+            print('Your password has been encoded and stored!')
+            pass_word = encode(password)
+        elif user_input == 2:
+            print('The encoded password is', pass_word, 'and the original password is', password)
+
+        
+        
+
+def encode(password):
+    temp_string = ''        
+    for i in password:
+        i = int(i)
+        i += 3
+        temp_string += str(i)
+    return temp_string
